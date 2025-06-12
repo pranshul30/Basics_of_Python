f7b06eb7b62c1c085b881b46d4b642eaf5cf284e
PRANSHUL SHARMA <144253627+pranshul30@users.noreply.github.com>
2025-06-12 16:04:38 +0530

    Add files via upload

---
+++ Class 4 Functions/F2.py
@@ -0,0 +1,4 @@
+def subtract(a, b):
+    return a - b
+
+print(subtract(b=5, a=10))
---
+++ Class 4 Functions/r.py
@@ -0,0 +1,7 @@
+#Recursion
+def fac(n):
+    if(n==1):
+        return 1
+    return n* fac(n-1)
+
+print(fac(3))
